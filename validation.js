d99c9de9ae24c432d306f60d002d9db2db04f1f2
NianziYi <nyi1@mail.sfsu.edu>
2021-11-20 14:08:24 +0800

    update form with self-designed captcha

--- validation.js
+++ validation.js
@@ -1,27 +1,65 @@
 //form validation
 (function () {
-    'use strict'
-  
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over form and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
+  'use strict'
 
-          //check if fields has correct input
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
+  var forms = document.querySelectorAll('.needs-validation')
 
-          //check if recaptcha is checked 
-          if (grecaptcha.getResponse() === '') {                            
-            event.preventDefault();
-            alert('Please prove you are not a Robot!');
-          }
+  // Loop over form and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
 
-          form.classList.add('was-validated')
-        }, false);
-      })
-  })()
+        //check if fields has correct input
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        if (!validCaptcha()) {
+          event.preventDefault()
+          alert('Please enter correct captcha!')
+        }
+
+        form.classList.add('was-validated')
+      }, false);
+    })
+})()
+
+
+//generate captcha randomly
+function captcha() {
+  var alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '%', '*', '&');
+
+  var i;
+
+  for (i = 0; i < 6; i++) {
+    var a = alpha[Math.floor(Math.random() * alpha.length)];
+    var b = alpha[Math.floor(Math.random() * alpha.length)];
+    var c = alpha[Math.floor(Math.random() * alpha.length)];
+    var d = alpha[Math.floor(Math.random() * alpha.length)];
+    var e = alpha[Math.floor(Math.random() * alpha.length)];
+    var f = alpha[Math.floor(Math.random() * alpha.length)];
+    var g = alpha[Math.floor(Math.random() * alpha.length)];
+  }
+
+  var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+  document.getElementById("codeCap").innerHTML = code
+  document.getElementById("codeCap").value = code
+}
+
+//check if user input matches the given captcha
+function validCaptcha() {
+  var string1 = removeSpaces(document.getElementById('codeCap').value);
+  var string2 = removeSpaces(document.getElementById('inputCap').value);
+  if (string1 == string2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function removeSpaces(string) {
+  return string.split(' ').join('');
+}
